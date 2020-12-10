99d0a4ccc7c54e88b6e4eb2100c471caf58a60dc
Bernardo Melo <bernardodmelo@gmail.com>
2020-12-10 16:02:30 +0000

    rezar

--- app.js
+++ app.js
@@ -35,17 +35,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 // Express View engine setup
-app.use(
+/*app.use(
   require("node-sass-middleware")({
     src: path.join(__dirname, "public"),
     dest: path.join(__dirname, "public"),
     sourceMap: true,
   })
-);
+);*/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+//app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));//
 app.use(
   session({
     secret: "pitagorasapp",
